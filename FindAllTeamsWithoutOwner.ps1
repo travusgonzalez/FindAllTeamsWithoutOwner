f71c099c5f9cb696fd2fdf627b7931f055e33ef1
Travus Gonzalez <travusgonzalez@outlook.com>
2022-12-29 19:01:17 -0500

    Initial Commit

---
+++ FindAllTeamsWithoutOwner.ps1
@@ -0,0 +1,28 @@
+Import-Module MicrosoftTeams
+Connect-MicrosoftTeams
+
+$listOfTeamsWithNoOwner = @()
+$listOfTeams = Get-Team
+
+foreach($team in $listOfTeams)
+{
+    # Write-Output "Finding all owners of team name: $team.DisplayName"
+    $listOfOwners = Get-TeamUser -GroupId $team.GroupId -Role Owner
+
+    if($listOfOwners.Length -eq 0)
+    {
+        # Write-Output "No owners found for $team.DisplayName"
+        # Populate object array needed to export to csv
+        $listOfTeamsWithNoOwner += New-Object PsObject -Property @{
+            'TeamName' = $team.DisplayName
+        }
+    }
+}
+
+# Save list to csv
+if($listOfTeamsWithNoOwner.Length -ne 0)
+{
+    Write-Output 'Exporting report...'
+    $listOfTeamsWithNoOwner | Export-Csv -Path 'C:\Dev\TeamsIssue\report.csv'
+    Write-Output 'Done!'
+}
