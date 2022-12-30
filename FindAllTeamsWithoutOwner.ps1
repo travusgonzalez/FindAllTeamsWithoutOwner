1aa1634a896545318c2b8b18057a808ca1f12944
Travus Gonzalez <travusgonzalez@outlook.com>
2022-12-29 19:57:29 -0500

    Updating script to check for default owner
    This code is largely untested but shows the general concept around running a report for all teams with a default owner assigned.

--- FindAllTeamsWithoutOwner.ps1
+++ FindAllTeamsWithoutOwner.ps1
@@ -1,28 +1,44 @@
 Import-Module MicrosoftTeams
 Connect-MicrosoftTeams
 
-$listOfTeamsWithNoOwner = @()
-$listOfTeams = Get-Team
+$outputArray = @()
 
+# Find all teams with no owner assigned
+$listOfTeamsWithNoOwner = Get-UnifiedGroup | Where-Object {([array](Get-UnifiedGroupLinks -Identity $_.Id -LinkType Owners)).Count -eq 0} `
+        | Select Id, DisplayName, ManagedBy, WhenCreated
+
+# Add a default owner to all teams without an owner
+foreach($team in $listOfTeamsWithNoOwner) { 
+    # Write-Host "Warning! The following group has no owner:" $team.DisplayName
+    Add-UnifiedGroupLinks -Identity $team.Id -LinkType Owners -Links 'defaultowner@domain.com'
+} 
+
+# Get a list of all teams
+$listOfTeams = Get-Team
 foreach($team in $listOfTeams)
 {
     # Write-Output "Finding all owners of team name: $team.DisplayName"
     $listOfOwners = Get-TeamUser -GroupId $team.GroupId -Role Owner
 
-    if($listOfOwners.Length -eq 0)
+    # If there is only one owner in the group
+    if($listOfOwners.Length -eq 1)
     {
-        # Write-Output "No owners found for $team.DisplayName"
-        # Populate object array needed to export to csv
-        $listOfTeamsWithNoOwner += New-Object PsObject -Property @{
-            'TeamName' = $team.DisplayName
+        # and the owner is the default owner
+        if($listOfOwners[0][0] -eq "defaultowner@domain.com")
+        {
+            # Write-Output "Default owner found for $team.DisplayName"
+            # Populate object array needed to export to csv
+            $outputArray += New-Object PsObject -Property @{
+                'TeamName' = $team.DisplayName
+            }
         }
     }
 }
 
 # Save list to csv
-if($listOfTeamsWithNoOwner.Length -ne 0)
+if($outputArray.Length -ne 0)
 {
     Write-Output 'Exporting report...'
-    $listOfTeamsWithNoOwner | Export-Csv -Path 'C:\report.csv'
+    $outputArray | Export-Csv -Path 'C:\report.csv'
     Write-Output 'Done!'
 }
